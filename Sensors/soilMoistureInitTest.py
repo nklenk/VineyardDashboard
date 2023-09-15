f7914dfbb8b014d79ab82ff772af73a48f450ba9
Neil Klenk <neil.klenk@gmail.com>
2023-09-15 11:41:22 -0500

    allow soil moisture sensors to run long term for testing

--- Sensors/soilMoistureInitTest.py
+++ Sensors/soilMoistureInitTest.py
@@ -26,31 +26,34 @@ ss = Seesaw(i2c_bus, addr=0x36) #determine address by looking at output of termi
 
 moistureReadings = []
 tempReadings = []
-while len(tempReadings) < 10:
+while True:
+    # collect 10 readings for an average
+    while len(tempReadings) < 10:
 
-    # read moisture level through capacitive touch pad
-    moisture = ss.moisture_read()
-    moistureReadings.append(moisture)
-    # read temperature from the temperature sensor
-    temp = ss.get_temp()
-    tempReadings.append(temp)
-    print("temp: " + str(temp) + "  moisture: " + str(moisture))
-    time.sleep(5)
+        # read moisture level through capacitive touch pad
+        moisture = ss.moisture_read()
+        moistureReadings.append(moisture)
+        # read temperature from the temperature sensor
+        temp = ss.get_temp()
+        tempReadings.append(temp)
+        # print("temp: " + str(temp) + "  moisture: " + str(moisture))
+        # sleep 5 se between readings
+        time.sleep(5)
 
-
-
-# Send to the table
-insertTemp = round(mean(tempReadings), 3)
-print("insertTemp: {}".format(insertTemp))
-insertMoisture = round(mean(moistureReadings), 3)
-print("insertMoisture: {}".format(insertMoisture))
-db = dBUtils.DBUtils()
-currentUuid = str(uuid.uuid4())
-hostname = socket.gethostname()
-currentTimestamp = datetime.datetime.now()
-insertQuery = "INSERT INTO moisturesensorreadings (UUID, HOSTNAME, TIMESTAMP, SOILMOISTURE, TEMPERATURE) VALUES ({}, {}, {}, {}, {})".format("'"+currentUuid+"'", "'"+hostname+"'", "'"+str(currentTimestamp)+"'", moisture, temp)
-print("insertQuery: {}".format(insertQuery))
-db.execute(insertQuery)
+    # Send to the table
+    insertTemp = round(mean(tempReadings), 3)
+    print("insertTemp: {}".format(insertTemp))
+    insertMoisture = round(mean(moistureReadings), 3)
+    print("insertMoisture: {}".format(insertMoisture))
+    db = dBUtils.DBUtils()
+    currentUuid = str(uuid.uuid4())
+    hostname = socket.gethostname()
+    currentTimestamp = datetime.datetime.now()
+    insertQuery = "INSERT INTO moisturesensorreadings (UUID, HOSTNAME, TIMESTAMP, SOILMOISTURE, TEMPERATURE) VALUES ({}, {}, {}, {}, {})".format("'"+currentUuid+"'", "'"+hostname+"'", "'"+str(currentTimestamp)+"'", moisture, temp)
+    print("insertQuery: {}".format(insertQuery))
+    db.execute(insertQuery)
+    # Sleep for 4 hrs between insertions
+    time.sleep(14400)
 
 
 
