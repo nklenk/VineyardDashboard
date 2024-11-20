49a218ede4a2c71ab06e49be9200f6625b5c738a
Neil Klenk <neil.klenk@gmail.com>
2024-11-20 06:47:28 -0600

    preparing to pull secrets from aws

---
+++ Resources/Secrets/secrets.py
@@ -0,0 +1,30 @@
+# source: AWS
+
+import boto3
+from botocore.exceptions import ClientError
+
+
+def get_secret():
+
+    secret_name = "rds!db-20056a1b-694f-4813-ade0-24d855f8c9b4"
+    region_name = "us-east-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+
+    # Your code goes here.
